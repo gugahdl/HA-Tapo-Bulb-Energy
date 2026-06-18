cb83f095b780c5e79ae3838c16ce5bda845536ed
Gustavo Duarte <61567876+gugahdl@users.noreply.github.com>
2026-06-18 14:03:55 -0300

    Add constants for Tapo Bulb Energy

---
+++ const.py
@@ -0,0 +1,7 @@
+"""Constants for Tapo Bulb Energy."""
+from datetime import timedelta
+
+DOMAIN = "tapo_bulb_energy"
+TPLINK_DOMAIN = "tplink"
+SCAN_INTERVAL = timedelta(seconds=300)
+CONF_TPLINK_ENTRY_ID = "tplink_entry_id"
